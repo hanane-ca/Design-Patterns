454e8f1d645bae5998942828b1d913ba35a2630e
elganiesta <54721994+elganiesta@users.noreply.github.com>
2020-11-21 09:56:36 +0100

    Init strategy

---
+++ strategy/src/InsertionSort.java
@@ -0,0 +1,7 @@
+class InsertionSort implements Strategy {
+
+    public void sort(int[] numbers) {
+        System.out.println("sorting array using insertion sort strategy");
+    }
+}
+
---
+++ strategy/src/MergeSort.java
@@ -0,0 +1,9 @@
+class MergeSort implements Strategy {
+
+    @Override
+    public void sort(int[] numbers) {
+        System.out.println("sorting array using merge sort strategy");
+
+    }
+}
+
